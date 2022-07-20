3e231433dbb7c091e732e72520458b82b07ecfb5
zygispr <99207910+zygispr@users.noreply.github.com>
2022-07-20 20:23:09 +0300

    Tenzies game done

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -1,21 +1,30 @@
 import { useState, useEffect } from "react";
 import Die from "./components/Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 export default function App() {
+  // Dice state (value, held/not held):
   const [dice, setDice] = useState(allNewDice());
+
+  // Tenzies game state (over/won, in progress/not won):
   const [tenzies, setTenzies] = useState(false);
 
+  // Roll count state (number of rolls):
+  const [rollCount, setRollCount] = useState(0);
+
+  // Checks if the game is over:
   useEffect(() => {
     const allHeld = dice.every((die) => die.isHeld);
     const allEqual = dice.every((die) => die.value === dice[0].value);
 
+    // If all dice are being held and they are of the same value, the game is won/over:
     if (allHeld && allEqual) {
       setTenzies(true);
-      console.log("You won!");
     }
   }, [dice]);
 
+  // This functions generates a new die:
   function generateNewDie() {
     return {
       value: Math.ceil(Math.random() * 6),
@@ -24,6 +33,7 @@ export default function App() {
     };
   }
 
+  // This function generates 10 new dice (it runs when the app first loads and when the user wins the game and starts a new game):
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
@@ -32,12 +42,21 @@ export default function App() {
     return newDice;
   }
 
+  // This functions is responsible for what happens after the user presses the "Roll" button (if the game is not yet over, pressing the button will generate a new die for each die that is not being held and increase the number of rolls by 1; if the game is over, pressing the button will generate a set of new random dice, reset the game state and the roll count):
   function rollDice() {
-    setDice((prevDice) =>
-      prevDice.map((die) => (die.isHeld ? die : generateNewDie()))
-    );
+    if (!tenzies) {
+      setDice((prevDice) =>
+        prevDice.map((die) => (die.isHeld ? die : generateNewDie()))
+      );
+      setRollCount((prevRollCount) => prevRollCount + 1);
+    } else {
+      setTenzies(false);
+      setDice(allNewDice());
+      setRollCount(0);
+    }
   }
 
+  // This function is responsible for setting the "isHeld" property:
   function holdDice(id) {
     setDice((prevDice) =>
       prevDice.map((die) =>
@@ -57,6 +76,9 @@ export default function App() {
 
   return (
     <main>
+      {tenzies && (
+        <Confetti width={window.innerWidth} height={window.innerHeight} />
+      )}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
@@ -64,8 +86,9 @@ export default function App() {
       </p>
       <div className="dice-container">{diceElements}</div>
       <button className="dice-roll" onClick={rollDice}>
-        Roll
+        {tenzies ? "New game" : "Roll"}
       </button>
+      <span className="rollcount">Number of rolls: {rollCount}</span>
     </main>
   );
 }
